4aab8d79e16333e2ad258ffc3a0924c262e9dd86
Michael.D.Y.N <dyn7517brs@outlook.com>
2017-05-04 09:20:48 +0800

    update

--- src/simpleFactoryPattern_1/Client.java
+++ src/simpleFactoryPattern_1/Client.java
@@ -1,22 +1,10 @@
 package simpleFactoryPattern_1;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class Client {
 	
 	public static void main(String[] args) {
 		Chart.getChart(XMLUtil.getChartType());
-//		List<String> list = new ArrayList<String>(); 
-//		list.add("aa");
-//		list.add("aa");
-//		list.add("aa");
-//		HashMap<String,Integer> value = new HashMap<String,Integer>();
-//		for(String a:list){
-//			value.put(a, value.get(a) == null?1:value.get(a) + 1);
-//			System.out.println(value.get(a));
-//		}
 	}
 	
 }
